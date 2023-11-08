adf8c96db9a86b55acbd4211a65e2caa4f00bce3
Forhadul Islam <mdforhadul44@gmail.com>
2023-11-08 11:13:02 +0600

    add slider

---
+++ diplearner-landing-app/src/Components/Home/BookShowSection.jsx
@@ -0,0 +1,17 @@
+import Slider from '@/Utils/Slider'
+import React from 'react'
+import ProducatCard from './ProducatCard'
+
+const BookShowSection = () => {
+  return (
+    <div className='max-w-[1280px] h-auto m-auto py-8'>
+
+      <div>
+        <Slider />
+      </div>
+      
+    </div>
+  )
+}
+
+export default BookShowSection
--- diplearner-landing-app/src/pages/index.js
+++ diplearner-landing-app/src/pages/index.js
@@ -1,11 +1,11 @@
+import BookShowSection from "@/Components/Home/BookShowSection";
 import HeroSection from "@/Components/Home/HeroSection";
-import SearchSection from "@/Components/Home/searchSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      <SearchSection />
+      <BookShowSection />
     </>
   );
 }
