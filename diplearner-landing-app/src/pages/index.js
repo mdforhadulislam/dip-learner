3fc2f8d2b5c1ffb0a9752cc3c04a905a749c2aab
Forhadul Islam <91374251+mdforhadulislam@users.noreply.github.com>
2023-11-08 10:12:34 +0600

    Update index.js

--- diplearner-landing-app/src/pages/index.js
+++ diplearner-landing-app/src/pages/index.js
@@ -1,5 +1,5 @@
 import HeroSection from "@/Components/Home/HeroSection";
-import SearchSection from "@/Components/Home/searchSection";
+import SearchSection from "@/Components/Home/SearchSection";
 
 export default function Home() {
   return (
