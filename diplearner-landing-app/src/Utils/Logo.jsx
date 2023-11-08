e6abd0ec4dae965ecccf5c249be2ef2ad1134e60
Forhadul Islam <91374251+mdforhadulislam@users.noreply.github.com>
2023-11-08 15:38:02 +0600

    Update Logo.jsx

--- diplearner-landing-app/src/Utils/Logo.jsx
+++ diplearner-landing-app/src/Utils/Logo.jsx
@@ -12,7 +12,7 @@ const Logo = ({ style, link, textStyle }) => {
     >
       <Image
         className={`${style || "w-auto h-10"}  mr-3`}
-        src={"./LOGO_3.png"}
+        src={"./image.png"}
         alt="Dip Learner Logo"
         width={"100%"}
         height={"100%"}
