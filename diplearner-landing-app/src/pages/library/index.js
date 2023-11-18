1efe9ef08817d697a73158e501e8788c172a553e
Forhadul Islam <91374251+mdforhadulislam@users.noreply.github.com>
2023-11-18 17:54:48 +0600

    Update index.js

--- diplearner-landing-app/src/pages/library/index.js
+++ diplearner-landing-app/src/pages/library/index.js
@@ -1,10 +1,13 @@
 import LibraryBody from "@/Components/Library/LibraryBody";
 import LibraryCategories from "@/Components/Library/LibraryCategories";
 import LibraryHead from "@/Components/Library/LibraryHead";
-
+import Head from "next/head";
 export default function BookLibrary() {
   return (
     <>
+     <Head>
+          <title>Hello world</title>
+        </Head>
       <LibraryHead />
       <LibraryCategories />
       <LibraryBody />
